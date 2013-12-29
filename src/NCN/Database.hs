acfc12689c10bbec2c30ba3bcc0bd1f9928eed41
ck101000xx <ck101000xx@gmail.com>
2013-12-29 23:25:13 +0800

    Update Database.hs

--- src/NCN/Database.hs
+++ src/NCN/Database.hs
@@ -6,7 +6,7 @@ module NCN.Database
   ) where
 
 import Control.Applicative(liftA2)
-import Control.Monad.Error.Class
+import Control.Monad.Error
 import Control.Monad.IO.Class(liftIO)
 import Control.Monad.Reader
 import Data.Either
@@ -18,14 +18,7 @@ instance ScottyError Failure where
   stringError = strMsg
   showError = show
 
-raiseLeft :: (ScottyError e, Monad m) => m (Either e a) -> ActionT e m a
-raiseLeft m = do
-  either <- lift m
-  case either of
-    Left e -> raise e
-    Right a -> return a
-
-doAction :: Action m a -> ReaderT C.DatabaseConfig m (Either Failure a)
+doAction :: Action m a -> ReaderT C.DatabaseConfig (ErrorT Failure m) a
 doAction action = do
   hostPort <- asks $ readHostPort . (liftA2 (++)  C.host ((':':) . C.port))
   db <- asks C.database
@@ -34,7 +27,7 @@ doAction action = do
     pipe <- runIOE $ connect hostPort
     result <- access pipe master db ((uncurry auth up) >> action)
     close pipe
-    return result
+    return $ ErrorT result
 
 tokens = "tokens"
 users = "users"
