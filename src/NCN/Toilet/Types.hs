b029c7566a0f2d26c9159a863e67f0cbcaa24530
ck101000xx <ck101000xx@gmail.com>
2014-01-30 01:29:31 +0800

    Use explicit export list Derive FromJSON ToJSON for Location type

--- src/NCN/Toilet/Types.hs
+++ src/NCN/Toilet/Types.hs
@@ -1,51 +1,38 @@
-{-# LANGUAGE OverloadedStrings, LambdaCase, DeriveDataTypeable #-}
-module NCN.Toilet.Types where
-import Control.Applicative
+{-# LANGUAGE OverloadedStrings, LambdaCase, DeriveDataTypeable, TemplateHaskell #-}
+module NCN.Toilet.Types
+  ( ToiletId
+  , Location(..)
+  , module U
+  ) where
 import Control.Monad
+import Data.Aeson.TH
 import Data.ByteString.Lazy(toStrict, fromStrict)
 import Data.Bson as B
 import Data.Text
 import Data.Typeable
-import qualified Data.UUID as U
+import Data.UUID as U
 
-class ToDocument a where
-  toDocument :: a -> B.Document
-
-class FromDocument a where
-  fromDocument :: B.Document -> Maybe a
-
-data Toilet = Toilet
-  { uuid :: U.UUID
-  , location :: Location
-  } deriving (Eq, Show)
-
-instance ToDocument Toilet where
-  toDocument = (<*>) [("uuid" =:) . uuid,  ("location" =:) . location] . pure
-
-instance FromDocument Toilet where
-  fromDocument d = Toilet <$> B.lookup "uuid" d <*> B.lookup "location" d
+type ToiletId = U.UUID
 
 data Location = Location
   { longitude :: Double
   , latitude :: Double
   } deriving (Eq, Show, Typeable)
 
-instance ToDocument Location where
-  toDocument (Location lo la) =
+deriveJSON defaultOptions ''Location
+
+instance Val Location where
+  val (Location lo la) = val
     [ "type" =: ("Point"::Text)
     , "coordinates" =: [lo, la]
     ]
-instance FromDocument Location where
-    fromDocument d = do
-      B.lookup "type" d >>= guard . (("Point" :: Text) ==)
-      B.lookup "coordinates" d >>= \case
-        [lo, la] -> return $ Location lo la
-        _        -> Nothing
+  cast' = cast' >=> \d -> do
+    B.lookup "type" d >>= guard . (("Point" :: Text) ==)
+    B.lookup "coordinates" d >>= \case
+      [lo, la] -> return $ Location lo la
+      _        -> Nothing
 
-instance B.Val Location where
-  val = B.val . toDocument
-  cast' = B.cast' >=> fromDocument
+instance Val U.UUID where
+  val = val . B.UUID . toStrict . U.toByteString
+  cast' = cast' >=> \case (B.UUID b) -> U.fromByteString (fromStrict b)
 
-instance B.Val U.UUID where
-  val = B.val . B.UUID . toStrict . U.toByteString
-  cast' = B.cast' >=> \case (B.UUID b) -> U.fromByteString (fromStrict b)
