fdc1e491e6601271ca518b106263e02568d764a9
ck101000xx <ck101000xx@gmail.com>
2014-01-30 01:34:24 +0800

    Add MongoDBConfig Type

--- src/NCN/Config.hs
+++ src/NCN/Config.hs
@@ -1,19 +1,30 @@
 {-# LANGUAGE TemplateHaskell #-}
-module NCN.Config where
-
+module NCN.Config(Config(..), ServerConfig(..), decodeFile) where
+import Control.Applicative
+import Data.Char
+import Data.Text(unpack)
 import Data.Yaml
+import Data.Aeson
 import Data.Aeson.TH
-import NCN.Config.Database
+import Database.MongoDB
 
 data Config = Config
   { server :: ServerConfig
-  , database :: DatabaseConfig
+  , mongodb :: MongoDBConfig
   } deriving (Show)
 
 data ServerConfig = ServerConfig
-  { host :: String
-  , port :: Int
-  } deriving(Show)
+  { serverHost :: String
+  , serverPort :: Int
+  } deriving (Show)
 
+data MongoDBConfig = MongoDBConfig
+  { mongoDBHost :: Host
+  , mongoDBDatabase :: Database
+  } deriving (Show)
+
+instance FromJSON Host where
+  parseJSON  = withText "Host" $ pure . readHostPort . unpack
 deriveFromJSON defaultOptions ''Config
-deriveFromJSON defaultOptions ''ServerConfig
+deriveFromJSON (defaultOptions{fieldLabelModifier = \s -> case drop (length "server") s of (c:cs) -> toLower c : cs }) ''ServerConfig
+deriveFromJSON (defaultOptions{fieldLabelModifier = \s -> case drop (length "mongoDB") s of (c:cs) -> toLower c : cs }) ''MongoDBConfig
