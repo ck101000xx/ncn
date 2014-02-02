41c291a9599caf951cbb78fdf9e7b434d207ca76
ck101000xx <ck101000xx@gmail.com>
2014-02-02 11:12:32 +0800

    new file:   src/NCN/MongoDB.hs

---
+++ src/NCN/MongoDB.hs
@@ -0,0 +1,10 @@
+module NCN.MongoDB where
+import Control.Monad.Reader
+import Database.MongoDB as M
+
+type RC = ReaderT MongoDBConfig
+connect :: RC IOE Pipe
+connect = asks mongoDBHost >>= lift . M.connect
+
+access :: AccessMode -> Action m a -> Pipe -> RC m (Either Failure a)
+access m a p = asks mongoDBDatabase >>= lift . ($a) . M.access p m
