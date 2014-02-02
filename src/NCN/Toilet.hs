54f2cc3f2bcb299ce3636e6f9edd5229b72ba411
ck101000xx <ck101000xx@gmail.com>
2014-02-02 11:35:20 +0800

    modified:   src/NCN/Toilet.hs

--- src/NCN/Toilet.hs
+++ src/NCN/Toilet.hs
@@ -8,25 +8,31 @@ import NCN.Toilet
 import NCN.Toilet.Types
 import NCN.MongoDB
 
+-- | Collection of toilets
 toilets :: Collection
 toilets = "toilets"
 
-insert :: (MonadIO m, Applicative m, Functor m) => ToiletLocation -> Action m ()
-insert = M.insert_ collection . toDocument
+-- | UUID of a toilet
+uuid :: Field
+uuid = "uuid"
 
-delete :: MonadIO m => ToiletId -> Action m ()
-delete id = deleteOne $ select ["uuid" =: id] toilets
+-- | Location of a toilet
+location :: Field
+location = "location"
 
-findInCircle :: (MonadIO m, MonadBaseControl IO m) => Double -> Location -> Action m Cursor
-findInCircle radius center = find query where
-  query = select
-    [ "location" =:
-      [ "$geoWithin" =:
-        ["$center" =: [val [longitude center, latitude center], val radius]]
-      ]
-    ] toilets
+-- | Create a new Toilet and return its UUID
+newToilet :: Action m ToiletId
+newToilet = do
+  id <- liftIO $ nextRandom
+  insert_ toilets [uuid =: id]
+  return id
 
-getLocation :: MonadIO m => ToiletId -> Action m (Maybe Location)
-getLocation id =  findOne query >>= return . (>>=M.lookup "location")
-  where query = (select ["uuid" =: id] toilets){project = ["location" =: (1::Int)]}
+-- | Query for Toilets within specified circle
+inCircle :: (Select s) => Double -> Location -> s
+inCircle radius center = select
+  [ location =:
+    [ "$geoWithin" =:
+      ["$center" =: [val [longitude center, latitude center], val radius]]
+    ]
+  ] toilets
 
