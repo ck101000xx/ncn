e1763a2318edb2b7e78f100db26c141546b2f149
ck101000xx <ck101000xx@gmail.com>
2014-02-03 12:48:55 +0800

    Swap the order of monad transformers

--- src/NCN/MongoDB.hs
+++ src/NCN/MongoDB.hs
@@ -1,12 +1,20 @@
 module NCN.MongoDB where
+import Control.Monad.Error
 import Control.Monad.Reader
+import Control.Monad.Morph
 import Control.Monad.Trans
 import Database.MongoDB as M
 import NCN.Config
 
-type RC = ReaderT MongoDBConfig
-connect :: RC IOE Pipe
-connect = asks mongoDBHost >>= lift . M.connect
+type ERC e m = ErrorT e (ReaderT Config m)
 
-access :: (MonadIO m) => AccessMode -> Action m a -> Pipe -> RC m (Either Failure a)
-access m a p = asks mongoDBDatabase >>= lift . ($a) . M.access p m
+connect :: MonadIO m => ERC IOError m Pipe
+connect = do
+  host <- lift $ asks (mongoDBHost . mongodb)
+  hoist liftIO $ M.connect host
+
+access :: MonadIO m => AccessMode -> Action m a -> Pipe -> ERC Failure m a
+access m a p = do
+  db <- lift $ asks (mongoDBDatabase . mongodb)
+  hoist lift . ErrorT $ M.access p m db a
+
