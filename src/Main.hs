6d4eea8bf67502f35a924bbfcbccb5a7b50834d8
ck101000xx <ck101000xx@gmail.com>
2014-02-07 23:42:00 +0800

    Create a early prototype

--- src/Main.hs
+++ src/Main.hs
@@ -1,38 +1,23 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty
-
-import NCN.Auth
-import NCN.Types
-import qualified NCN.Config as C
+module Main where
 import Control.Monad.Reader
-
-import Network.HTTP.Conduit(newManager)
+import Data.Yaml
+import NCN.App
+import NCN.Config
+import Web.Scotty.Trans
 
 main = do
-  maybeConfig <- decodeFile ""
-  case maybeConfig of
-    Just Config -> app config
-    Nothing -> putStrLn "Invalid config"
-  
-app config = scotty 3000 $ do
-  get "/" $ do
-    html "<h1>Hello World</h1>"
-  post "/tokens" $ do
-    maybeEmail <- param "assertion" >>= liftIO getEmail
-    case maybeEmail of
-      Just email ->
-        token <- liftIO . runReaderT (C.acid config) $ do
-          ensureUser email
-          getNewToken email
-        text token
-      Nothing -> status status401
-  delete "/tokens/:t" $ do
-    a <- getEmail
-    param "t" >>= liftIO . runReaderT (C.mongoDB config) $ deleteToken t
-  where
-    auth = do
-      token <- param "token"
-      
-checkAss assertion = newManager def >>= checkAssertion audience assertion
-  where
-    audience = "http://ck101000xx.kd.io"
+  decodeFileEither "./cfg.yaml" >>= either (putStrLn . show) (runReaderT $ ncnScottyT handleToilets)
+
+ncnScottyT :: ScottyT e (ReaderT Config IO) () -> ReaderT Config IO ()
+ncnScottyT s = do
+  port <- asks (serverPort . server)
+  cfg <- ask
+  scottyT port id (flip runReaderT cfg) s
+
+{- 
+ncnScottyAppT :: (Monad m) => ScottyT (ReaderT Config m) () -> ReaderT Config m Application
+ncnScottyAppT = do
+  cfg <- ask
+  scottyAppT id (runReaderT cfg)
+-}
---
+++ src/NCN/App.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
+module NCN.App where
+import Control.Applicative
+import Control.Monad.Error
+import Control.Monad.Reader
+import Control.Monad.Trans
+import Data.Maybe
+import Data.Text.Lazy hiding(find)
+import Data.UUID.V4
+import Database.MongoDB hiding(connect, access, UUID)
+import NCN.Config
+import NCN.MongoDB
+import NCN.Toilet
+import Network.HTTP.Types.Status
+import Web.Scotty.Trans as ST
+
+handleToilets :: ScottyT Text (ReaderT Config IO) ()
+handleToilets = do
+  post "/toilets" $ do
+    location' <- Location <$> param "location[latitude]" <*> param "location[longitude]"
+    name' <- param "name"
+    let doc = [location =: (location' :: Location), name =: (name' :: Name)]
+        run = do
+          id <- liftIO nextRandom
+          insert_ toilets $ (uuid =: id):doc
+          return id
+    id <- raiseET connect >>= fmap raiseET (access master run)
+    json . toString $ id
+  get "/toilets/:uuid" $ do
+    u <- param "uuid"
+    let run = findOne $ select [uuid =: (u :: UUID)] toilets
+    mDoc <- raiseET connect >>= fmap raiseET (access slaveOk run)
+    if isNothing mDoc then status status404 else text "Oh Yeah"
+  get "/toilets" $ do
+    radius <- param "circle[radius]"
+    center <- Location <$> param "circle[longitude]" <*> param "circle[latitude]"
+    let run = fmap (fmap $ at uuid) $ find (inCircle radius center){project = [uuid =: (1 :: Int)]} >>= rest
+    ids <- raiseET connect >>= fmap raiseET (access slaveOk run)
+    json . (fmap toString) $ ids
+
+raiseET :: (Show e, Monad m) => ErrorT e m a -> ActionT Text m a
+raiseET = lift . runErrorT >=> either raiseShow return
+
+raiseShow :: (Show s, Monad m) => s -> ActionT Text m a
+raiseShow = raise . pack . show
+
+instance Parsable UUID where
+  parseParam = fmap (maybe (Left "parseParam UUID: no parse") return) $ fromString . unpack
