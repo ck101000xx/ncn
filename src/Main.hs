11b7f9da5d552100ff6a3c278ee86acd10d85039
ck101000xx <ck101000xx@gmail.com>
2014-02-08 20:06:31 +0800

    Read the path of config file from env

--- src/Main.hs
+++ src/Main.hs
@@ -4,10 +4,12 @@ import Control.Monad.Reader
 import Data.Yaml
 import NCN.App
 import NCN.Config
+import System.Environment
 import Web.Scotty.Trans
 
 main = do
-  decodeFileEither "./cfg.yaml" >>= either (putStrLn . show) (runReaderT $ ncnScottyT handleToilets)
+  configPath <- getEnv "NCN_CONFIG_PATH"
+  decodeFileEither configPath >>= either (putStrLn . show) (runReaderT $ ncnScottyT handleToilets)
 
 ncnScottyT :: ScottyT e (ReaderT Config IO) () -> ReaderT Config IO ()
 ncnScottyT s = do
