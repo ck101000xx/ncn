8b0ce3ffeb83ed277fa58e9fece2759d24db1ed4
ck101000xx <ck101000xx@gmail.com>
2014-02-09 13:21:16 +0800

    Read config form openshift env variables

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,8 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 import Control.Monad.Reader
-import Data.Yaml
 import NCN.App
-import NCN.Config
-import System.Environment
-import Web.Scotty.Trans
+import NCN.Config.Openshift
+import NCN.Scotty
 
-main = do
-  configPath <- getEnv "NCN_CONFIG_PATH"
-  decodeFileEither configPath >>= either (putStrLn . show) (runReaderT $ ncnScottyT handleToilets)
-
-ncnScottyT :: ScottyT e (ReaderT Config IO) () -> ReaderT Config IO ()
-ncnScottyT s = do
-  port <- asks (serverPort . server)
-  cfg <- ask
-  scottyT port id (flip runReaderT cfg) s
-
-{- 
-ncnScottyAppT :: (Monad m) => ScottyT (ReaderT Config m) () -> ReaderT Config m Application
-ncnScottyAppT = do
-  cfg <- ask
-  scottyAppT id (runReaderT cfg)
--}
+main :: IO ()
+main = getConfig >>= (runReaderT $ ncnScottyT handleToilets)
